0eba8e319abce4d34dfbd8c0c2771419f6f99154
tschmidt <devteam@dev-shiny.corp.tripractix.com>
2009-03-04 18:15:22 -0500

    Setting up jqueried slider for examples

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,14 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+$(function () {
+	
+	// 1. Get the example tab links
+	//    1a. Watch for click and show the corresponding section
+	//    1b. Remove "active" class from all other tab links and apply to the clicked
+	//		 link.
+	//
+	// 2. When the user selects an example check to see if it is the current example.
+	//    If it is ignore the click, otherwise slide the current example out of view
+	//    and slide the selected one into view.
+	
+})
