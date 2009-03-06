a30b3ac947233ba7069076cf78de8a7b3e493268
tschmidt <devteam@dev-shiny.corp.tripractix.com>
2009-03-06 15:40:42 -0500

    Completed the nav on the overview page. Added a bit more styling to the site.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -2,13 +2,38 @@
 // This file is automatically included by javascript_include_tag :defaults
 $(function () {
 	
-	// 1. Get the example tab links
-	//    1a. Watch for click and show the corresponding section
-	//    1b. Remove "active" class from all other tab links and apply to the clicked
-	//		 link.
-	//
-	// 2. When the user selects an example check to see if it is the current example.
-	//    If it is ignore the click, otherwise slide the current example out of view
-	//    and slide the selected one into view.
+	if ( $("#examples_nav").length > 0 ) {
+		
+		var $tabs = $("#examples_nav a"),
+			$example_container = $("#examples"),
+			$examples = $(".example");
+			
+		$examples.hide();
+		
+		$tabs.click(function (e) {
+			
+			e.preventDefault();
+			
+			var $this = $(this)
+			
+			$tabs.removeClass("active");
+			
+			$this.addClass("active");
+			
+			$examples.fadeOut("fast");
+			
+			$example_container.animate({
+				height : $( $this.attr("href") ).outerHeight()
+			}, 350, function () {
+				var $example = $($this.attr("href"));
+				
+				$example.fadeIn();
+				$example.css({ position : "absolute", top : 0 })
+			});
+			
+		});
+		
+		$tabs.filter(":first").click();
+	}
 	
 })
